0e31c8de1d8d519c81ac53c99445537ae6382835
vladimir <vladimis1989@mail.ru>
2022-10-21 22:16:00 +0300

    relise1 Transfer delegate

--- TransferInfo/SceneDelegate.swift
+++ TransferInfo/SceneDelegate.swift
@@ -11,13 +11,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    // организуем в SceneDelegate вью контроллер при запуске
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+// 1- создаём сцену
+        guard let windowScene = (scene as? UIWindowScene) else { return  }
+// 2 - должны активировать(инициализировать) свойство var window: UIWindow? что выше
+// с помощью let windowScene
+        window = UIWindow(windowScene: windowScene)
+
+// добавляем вью контроллер на вью
+// создаем свойства и укзаываем что в нём корневой котнорллер будет ViewController
+        let nav = UINavigationController(rootViewController: ViewController())
+        nav.isNavigationBarHidden = true //скрываем меню на навигатион котроллере
+//делаем его корневым
+        window?.rootViewController = nav // делаем корневым
+        window?.makeKeyAndVisible() // добавляем на кран
         guard let _ = (scene as? UIWindowScene) else { return }
     }
+
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
