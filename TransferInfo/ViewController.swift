ec66c7866647495d78b4f0881a72d4d605e671c1
vladimir <vladimis1989@mail.ru>
2022-10-21 22:09:11 +0300

    Initial Commit

---
+++ TransferInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransferInfo
+//
+//  Created by Vladimir Grishchenkov on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
